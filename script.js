01fa897b5a08268e9a8b272638924239b1e001fc
Anastasia <nastya.proskuryakova.98@mail.ru>
2024-04-15 16:34:04 +0300

    preloader added

--- script.js
+++ script.js
@@ -10,26 +10,34 @@ window.addEventListener("load", () => {
   header.classList.add("scale-in-ver-top");
 });
 
-
 // const mainBox = document.querySelector("main");
 // window.addEventListener('load', () => {
 //     mainBox.classList.add("scale-up-center");
 // })
 
-// burger-menu mobile 
+// burger-menu mobile
 const hamb = document.getElementById("hamburger");
 const closeIcon = document.querySelector(".closeMenu");
 hamb.addEventListener("click", hambHandler);
 closeIcon.addEventListener("click", closeMenu);
 function hambHandler(e) {
   e.preventDefault();
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('show');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
 }
 
 function closeMenu(e) {
   e.preventDefault();
-  const menu = document.getElementById('menu');
-  menu.classList.toggle('show');
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
 }
 
+//preloader
+
+window.onload = function () {
+  document.body.classList.add("loaded_hiding");
+  window.setTimeout(function () {
+    document.body.classList.add("loaded");
+    document.body.classList.remove("loaded_hiding");
+  }, 500);
+};
